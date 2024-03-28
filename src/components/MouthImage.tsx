b074fac0dd0b5c3565c9ebb96d0f19179724bf8d
Loren Couse <couselm@gmail.com>
2024-03-28 14:45:49 +0800

    v0.1
    Add sore location selection.
    Add size pain level adjustment sliders.

--- src/components/MouthImage.tsx
+++ src/components/MouthImage.tsx
@@ -1,27 +1,83 @@
 import React, { useState } from "react";
 import './MouthImage.css';
-import mouthDiagram from '../assets/images/mouth-diagram.png';
+import mouthDiagram from '../assets/images/mouthDiagramNoLabels.png';
 
 
 const MouthImage: React.FC = () => {
-    const [dotPosition, setDotPosition] = useState<{ x: number; y: number } | null>(null);
+    const [sorePosition, setSorePosition] = useState<{ x: number; y: number } | null>(null);
+    const [soreSize, setSoreSize] = useState(3);
+    const [sorePainLevel, setSorePainLevel] = useState(3);
+
 
     const handleImageClick = (event: React.MouseEvent<HTMLImageElement, MouseEvent>) => {
-        const rect = event.currentTarget.getBoundingClientRect();
-        const x = event.clientX - rect.left;
-        const y = event.clientY - rect.top;
-        setDotPosition({ x, y })
-        
-    }
+    const rect = event.currentTarget.getBoundingClientRect();
+    const x = event.clientX - rect.left; 
+    const y = event.clientY - rect.top; 
+    setSorePosition({ x, y }); 
+    };
 
-        const handleContextMenu = (event: React.MouseEvent) => {
+    const handleContextMenu = (event: React.MouseEvent) => {
         event.preventDefault();
     };
 
+    const handleSizeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSoreSize(parseInt(event.target.value, 10));
+    }
+
+    const handlePainLevelChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSorePainLevel(parseInt(event.target.value, 10));
+    }
+
+    const getColor = (step: number) => {
+        const value = step * 28;
+        return `rgb(${value}, ${255 - value}, ${255 - value})`;
+    };
+
     return (
-        <div className="mouth-image" onClick={handleImageClick} onContextMenu={handleContextMenu}>
-            <img src={mouthDiagram} alt="clickable" />
-            {dotPosition && (<div className="canker-sore" style={{left: dotPosition.x, top: dotPosition.y}}></div>)}
+        <div className="mouth-image-container" onContextMenu={handleContextMenu}>
+            <img src={mouthDiagram} alt="Mouth Diagram" onClick={handleImageClick}/>
+            {sorePosition && (
+            
+            <div className="canker-sore" 
+            style={{
+            left: sorePosition.x, 
+            top: sorePosition.y, 
+            width: `${soreSize}px`,
+            height: `${soreSize}px`,
+            backgroundColor: getColor(sorePainLevel),
+            }}></div>
+            )}
+
+
+        <div className="sliders">
+            <div className="slider-container">
+            <label>
+                Sore Size: {soreSize} mm 
+                <input type="range" className="slider" min="1" max="20" value={soreSize} onChange={handleSizeChange} />
+
+            </label>
+            </div>
+            
+            <div className="slider-container">
+            <label>
+                Pain Level: {sorePainLevel}
+                <input type="range" className="slider" min="1" max="10" value={sorePainLevel} onChange={handlePainLevelChange} />
+            </label>
+            </div>
+
+        </div>
+
+        <div className="navigation-buttons">
+            <button>
+                Finish
+            </button>
+
+            <button>
+                Add More
+            </button>
+
+        </div>
+
 
         </div>
 
---
+++ src/components/MouthZoneSelector.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import "./MouthImage.css"
+
+
+const MouthZoneSelector: React.FC = () => {
+    
+    return (
+        <div className=".mouth-image-container" >
+            <img src="../assets/images/mouth-diagram.png" alt="Mouth Zones"/>
+        </div>
+    );
+
+
+};
